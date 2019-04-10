a34ea1cc53103eeb3758f40d2206599baf83ea06
MicheleMorelli <michele.morelli@london.ac.uk>
2019-04-10 15:42:27 +0000

    new structure, some bug fixing

--- outage/__init__.py
+++ outage/__init__.py
@@ -4,7 +4,7 @@ import requests
 import os
 import json
 from requests.auth import HTTPBasicAuth
-
+from . import cw_connector
 import azure.functions as func
 
 # Create a ticket in ConnectWise on receipt of Outage Information
@@ -38,88 +38,20 @@ def receive_outage(req):
     logging.info(f"Received: {params}")
     # check all params are present
     if 'Company_name' in params and 'outage_id' in params and 'fqdn' in params and 'reason' in params and 'services' in params and 'items' in params and 'starttime' in params:
-        ticket_exists = find_ticket(params['outage_id'])
+        ticket_exists = cw_connector.find_ticket(params['outage_id'])
         # check whether ticket exists
-        if ticket_exists != None:
+        if ticket_exists:
             return { 
                 "message": f"Ticket exists {ticket_exists[0]['id']}",
                 "status": 200
             }
         else:
-            return create_ticket(params)
+            return cw_connector.create_ticket(params)
+
     else:
         return { 
             "message": "Missing one or more required parameters",
             "status": 422
         }
 
-def company_name(custid):
-    logging.info('Get Company ID')
-    path = '/company/companies?conditions=identifier="' + custid + '"'
-    try:
-        headers = {'content-type': 'application/json', 'accept': 'application/json'}
-        r = requests.get( cw_uri() + path , headers=headers, auth=(cw_user(), cw_key()) )
-        return r.json()[0]['id']
-    except Exception as e:
-        logging.error(f"Company request failed {e}")
-        return int(os.getenv('CW_CATCHALL'))
 
-def find_ticket(outage_id):
-    logging.info('Find Ticket')
-    try:
-        headers = {'content-type': 'application/json', 'accept': 'application/json'}
-        r = requests.get( f"{cw_uri()}/service/tickets?customFieldConditions=caption=\"Outage ID\" AND value = {outage_id}" , headers=headers, auth=(cw_user(), cw_key()) )
-        return r.json()
-    except Exception as e:
-        logging.error(f"Ticket find Failed {e}")
-        None
-    
-def create_ticket(params):
-    logging.info('Create Ticket')
-    try:
-        headers = {'content-type': 'application/json', 'accept': 'application/json'}
-        desc = f"Item(s): {params['items']} \nServices: {params['services']} \nReasons: {params['reason']}\nBegan at: {params['starttime']}"
-        body = { 
-         "summary": f"Panopta Alert on {params['fqdn']}" ,
-         "company": { "id": int(company_name(params['Company_name'])) }, 
-         "recordType": "ServiceTicket", 
-         "board": { "id": int(service_board()) },
-         "description": desc,
-         "customFields": [
-             {
-                'id': 60, 
-                'caption': 'Outage ID', 
-                'type': 'Text', 
-                'entryMethod': 'EntryField', 
-                'numberOfDecimals': 0,
-                'value': params['outage_id']
-             }
-            ]
-        }
-        data = json.dumps(body)
-        r = requests.post( f"{cw_uri()}/service/tickets" , headers=headers, data=data, auth=(cw_user(), cw_key()) )
-        return { 
-            "message": str(r.json()),
-            "status": r.status_code
-        } 
-    except Exception as e:
-        logging.error(f"Ticket creation Failed {e}")
-        return { 
-            "message": f"Ticket creation Failed {e}",
-            "status": 500
-        }
-        
-def service_board():
-    return os.getenv('CW_SERVICE_BOARD')
-
-def cw_key():
-    return os.getenv('CW_KEY')
-
-def cw_user():
-    return os.getenv('CW_USER')
-    
-def cw_uri():
-    return os.getenv('CW_URI')
-    
-def cw_catchall():
-    return os.getenv('CW_CATCHALL')

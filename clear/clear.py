54329607167b2434f7090a3a87eae4f7968bbcf2
MicheleMorelli <michele.morelli@london.ac.uk>
2019-04-14 22:42:36 +0000

    clear branch

--- clear/clear.py
+++ clear/clear.py
@@ -8,11 +8,11 @@ import connectwise.connect as cw
 
 def run(req: REQUEST) -> RESPONSE:
     logging.info('Python HTTP trigger function processed a clear request.')
-    return allow_request(req) if req.method == "POST" else block_request()
+    return allow_request(req) if req.method == "PATCH" else block_request()
 
 
 def allow_request(req: REQUEST)->RESPONSE:
-    outage = get_outage(req)
+    outage = get_clear(req)
     return RESPONSE(outage['message'], status_code=outage['status'])
 
 
@@ -21,12 +21,49 @@ def block_request(req: REQUEST)->RESPONSE:
     return RESPONSE(message,status_code=405)
 
 
-def get_outage(req: REQUEST) -> Dict:
-    logging.info('Outage Received') 
+def get_clear(req: REQUEST) -> Dict:
+    logging.info('Clear Received') 
     params = req.get_json()
     logging.info(f"Received: {params}")
     # check if any of the required params is missing
     if not all(param in params for param in panopta_required_params()):
         return missing_param_response()
     ticket_exists = cw.find_ticket(params['outage_id'])
-    return existing_ticket(ticket_exists) if ticket_exists else cw.create_ticket(params)
+    return cw.close_ticket(params) if ticket_exists else non_existing_ticket()
+
+
+'''
+Returns a list of the params required by Panopta
+'''
+def panopta_required_params()->List[str]:
+    return ['Company_name', 
+            'outage_id',
+            'fqdn',
+            'reason',
+            'services',
+            'items',
+            'cleartime',
+            'duration'
+            ]
+
+
+
+def existing_ticket(ticket: Dict)->Dict:
+    return { 
+        "message": f"Ticket exists {ticket[0]['id']}",
+        "status": 200
+        }
+
+
+def non_existing_ticket()->Dict:
+    return { 
+        "message": f"Ticket does not exists!",
+        "status": 500
+        }
+
+def missing_param_response()->Dict:
+    return { 
+        "message": "Missing one or more required parameters",
+        "status": 422
+        }
+

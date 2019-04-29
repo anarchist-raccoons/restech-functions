b2a913777f38121413e465248e0419f9be60dae9
MicheleMorelli <michele.morelli@london.ac.uk>
2019-04-29 22:50:51 +0000

    rethinking the outage function to work asynchronously

--- outage/__init__.py
+++ outage/__init__.py
@@ -1,4 +1,16 @@
+import threading
 from . import outage
 
 def main(req: outage.REQUEST) -> outage.RESPONSE:
-    return outage.run(req)
+    thread = threading.Thread(target=outage.run, args=(req,))
+    thread.start()
+    return webhook_response() 
+
+
+
+def webhook_response()->outage.RESPONSE:
+    response = outage.RESPONSE(
+            "Panopta outage webhook request received",
+            status_code=200)
+    return response
+
